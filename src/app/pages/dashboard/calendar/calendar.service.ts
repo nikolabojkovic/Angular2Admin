369b43669be9f12359406bb156a31e8f2bf41e9a
Nikola <nikolabojkovic@gmail.comm>
2018-02-25 19:07:38 +0100

    calendar buttons fix, edit event from calendar implemented

--- src/app/pages/dashboard/calendar/calendar.service.ts
+++ src/app/pages/dashboard/calendar/calendar.service.ts
@@ -12,7 +12,7 @@ export class CalendarService {
     let dashboardColors = this._baConfig.get().colors.dashboard;
     return {
       header: {
-        left: 'prev,next today',
+        left: 'prevYear,prev today next,nextYear',
         center: 'title',
         right: 'month,agendaWeek,agendaDay'
       },
